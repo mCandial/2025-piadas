6097231d711212adbe89f3e1d9f420a097bcbdbd
Daniel Correa Lobato <daniel@lobato.org>
2024-08-21 22:10:35 -0300

    Scaffold do controller de Piadas

---
+++ Models/Piada.cs
@@ -0,0 +1,13 @@
+namespace Piadas.Models;
+
+public class Piada
+{
+    public int Id { get; set; }
+    public string Pergunta { get; set; }
+    public string Resposta { get; set; }
+
+    public Piada()
+    {
+        
+    }
+}

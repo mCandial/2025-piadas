f4f8bde8fa8e16fea772247bb1d1a70fdcd8e03c
mCandial <matheuscandial@outlook.com>
2024-09-04 20:18:31 -0300

    Versão final da Aplicação

--- Migrations/20240828232044_Inicio.cs
+++ Migrations/20240828232044_Inicio.cs
@@ -5,7 +5,7 @@
 namespace Piadas.Migrations
 {
     /// <inheritdoc />
-    public partial class Iniciodaaplicação : Migration
+    public partial class Inicio : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
